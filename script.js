3b86d2b2662148809dd8d299b10ae14f577d7b2f
jinzoon <186304379+jinzoon@users.noreply.github.com>
2025-08-21 18:26:40 +0900

    Create operate function
    In preparation for a user interface, I created a function that takes two
    numbers and an operator value. It then calls the correct function for
    that operator and returns the calculated value.

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-console.log ("1+1=" + add(1,1) );
-console.log ("2+1=" + add(2,1) );
-console.log ("100+115=" + add(100,115) );
-console.log ("8+0=" + add(8,0) );
+let firstNum;
+let secondNum;
+let operator;
 
-console.log ("1*1=" + multiply(1,1) );
-console.log ("2*1=" + multiply(2,1) );
-console.log ("100*115=" + multiply(100,115) );
-console.log ("8*0=" + multiply(8,0) );
+console.log ("1+1=" + operate("add",1,1) );
+console.log ("2+1=" + operate("add",2,1) );
+console.log ("100+115=" + operate("add",100,115) );
+console.log ("8+0=" + operate("add",8,0) );
+console.log ("1*1=" + operate("mult",1,1) );
+console.log ("2*1=" + operate("mult",2,1) );
+console.log ("23*34=" + operate("mult",23,34) );
+console.log ("8/0=" + operate("div",8,0) );
+console.log ("1/1=" + operate("div",1,1) );
+console.log ("2-1=" + operate("sub",2,1) );
+console.log ("100-115=" + operate("sub",100,115) );
+console.log ("8-S0=" + operate("sub",8,0) );
 
 
-console.log ("1-1=" + subtract(1,1) );
-console.log ("2-1=" + subtract(2,1) );
-console.log ("100-115=" + subtract(100,115) );
-console.log ("8-0=" + subtract(8,0) );
-
-
-console.log ("1/1=" + divide(1,1) );
-console.log ("2/1=" + divide(2,1) );
-console.log ("100/115=" + divide(100,115) );
-console.log ("8/0=" + divide(8,0) );
+function operate(operator, firstNum, secondNum){
+    if (operator == "add"){
+       return add(firstNum,secondNum);
+    }
+    else if (operator == "sub") {
+        return subtract(firstNum,secondNum);
+    }
+    else if (operator == "mult") {
+        return multiply(firstNum,secondNum);
+    }
+    else if (operator == "div"){
+        return divide(firstNum, secondNum);
+    }
+    else {
+        return null;
+    }
+}
 
 
 function add(num1,num2) {
