4061c54fe579978171d97d556418159a8f879397
jinzoon <186304379+jinzoon@users.noreply.github.com>
2025-08-22 19:31:20 +0900

    refactor(js): remove console.log() lines
    - Removed the console.log() tests
    - These were only used for logic testing and no longer serve a purpose

--- script.js
+++ script.js
@@ -2,20 +2,6 @@ let firstNum;
 let secondNum;
 let operator;
 
-console.log ("1+1=" + operate("add",1,1) );
-console.log ("2+1=" + operate("add",2,1) );
-console.log ("100+115=" + operate("add",100,115) );
-console.log ("8+0=" + operate("add",8,0) );
-console.log ("1*1=" + operate("mult",1,1) );
-console.log ("2*1=" + operate("mult",2,1) );
-console.log ("23*34=" + operate("mult",23,34) );
-console.log ("8/0=" + operate("div",8,0) );
-console.log ("1/1=" + operate("div",1,1) );
-console.log ("2-1=" + operate("sub",2,1) );
-console.log ("100-115=" + operate("sub",100,115) );
-console.log ("8-S0=" + operate("sub",8,0) );
-
-
 function operate(operator, firstNum, secondNum){
     if (operator == "add"){
        return add(firstNum,secondNum);
